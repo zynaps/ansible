28d4c794e75bce38d4e824330a6fd37c5d89694e
Igor Vinokurov <zynaps@zynaps.ru>
2021-10-02 11:16:59 +0400

    added script to register new host

---
+++ bin/host_add.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+host=$2
+domain=zynaps.ru
+ip=$1
+endpoint=$3
+port=51820
+
+if [ ! -f host_vars/$host.$domain.yml ]; then
+    public_key=`wg genkey | tee $host.key | wg pubkey`
+    private_key=`ansible-vault encrypt_string --vault-password-file ./.vault_password --encrypt-vault-id default --output - < $host.key`
+    rm -f $host.key
+
+    cat << EOF > host_vars/$host.$domain.yml
+---
+wireguard:
+  ip: $ip
+  private_key: $private_key
+  public_key: $public_key
+  endpoint: $endpoint:$port
+EOF
+fi
+
+if [ ! -f $host.$domain.yml ]; then
+    cat << EOF > $host.$domain.yml
+---
+- name: $host setup
+  hosts: $host.$domain
+  become: yes
+
+EOF
+fi
